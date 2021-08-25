9b9d5101cbbe3d171ea4610ece67eb9e02c56e8e
MoHadi1976 <m_hadi24@hotmail.com>
2021-08-25 16:20:48 +0100

    New commit, as I  deleted Repo

---
+++ app.js
@@ -0,0 +1,68 @@
+var scores, roundScore, activePlayer, dice, gamePlaying = true;
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'Images/Dice-' + dice + '.png';
+
+        if (dice !== 1){
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }
+        else{
+            document.querySelector('#name-' + activePlayer).textContent = 'You lose!';
+           gamePlaying = false;
+           }
+           
+           
+           
+    if(gamePlaying){
+        scores[activePlayer] += dice;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+        if (scores[activePlayer] >= 21) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }
+    }
+    nextPlayer();
+    
+}
+});
+
+
+function nextPlayer(){
+    
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0 ;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+
+document.querySelector('.btn-new').addEventListener('click', init);
